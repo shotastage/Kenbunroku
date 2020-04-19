5d7a7ccd50801833c62c749cad88c35c6a704757
Shota Shimazu <hornet.live.mf@gmail.com>
2020-04-19 16:08:23 +0900

    Initial Commit

---
+++ Kenbunroku/Kenbunroku.h
@@ -0,0 +1,19 @@
+//
+//  Kenbunroku.h
+//  Kenbunroku
+//
+//  Created by Shota Shimazu on 2020/04/19.
+//  Copyright © 2020 Shota Shimazu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Kenbunroku.
+FOUNDATION_EXPORT double KenbunrokuVersionNumber;
+
+//! Project version string for Kenbunroku.
+FOUNDATION_EXPORT const unsigned char KenbunrokuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kenbunroku/PublicHeader.h>
+
+
