08dda65cb4c138ad4354c2f00fcbc3753f1bca4b
Shota Shimazu <hornet.live.mf@gmail.com>
2020-04-21 00:46:15 +0900

    revise

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+   
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let html: String = """
+           <html>
+           <body>
+               <h1>Hello, world</h1>
+           </body>
+           </html>
+           """
+           
+        let data = Data(html.utf8)
+        
+        if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
+               label.attributedText = attributedString
+           }
     }
-
 
 }
 
