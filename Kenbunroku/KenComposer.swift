3f13f85cf678660548fe06f38b9f850882b334d5
Shota Shimazu <hornet.live.mf@gmail.com>
2020-04-24 16:22:48 +0900

    add base class

--- Kenbunroku/KenComposer.swift
+++ Kenbunroku/KenComposer.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2020 Shota Shimazu. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class KenReaderController: UIViewController {
+    
+    
+}
+#imageLiteral(resourceName: "Screen Shot 2020-04-24 at 1.50.45.png")
--- Kenbunroku/KenSection.swift
+++ Kenbunroku/KenSection.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2020 Shota Shimazu. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class KenSection: UIView {
+    
+}
