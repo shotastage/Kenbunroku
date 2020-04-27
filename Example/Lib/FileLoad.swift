23292813009575a3aeb27b7251005dbfd247000b
Shota Shimazu <hornet.live.mf@gmail.com>
2020-04-27 16:05:22 +0900

    update sample app

---
+++ Example/Lib/FileLoad.swift
@@ -0,0 +1,13 @@
+//
+//  FileLoad.swift
+//  Example
+//
+//  Created by Shota Shimazu on 2020/04/23.
+//  Copyright © 2020 Shota Shimazu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
